408e3f59099ed7d2dc6252ad5e063d999436dba2
Eli4545 <elisimon4545@gmail.com>
2024-08-14 23:03:52 -0400

    created the Post,patch and delete rouutes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,8 @@
-import { Body, Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-    /*
-    GET /users
-    GET / users/:id
-    POST /users
-    PATCH /users/:id
-    DELETE /users/:id
-    
-    */ 
+     
 
     @Get() // GET /users
     findAll() {
@@ -20,6 +13,21 @@ export class UsersController {
 
     @Get (':id') // GET /users/;id
     findOne(@Param(':id') id: string) {
+        return { id }
+    }
+
+    @Post()
+    create(@Body() user: {}) {
+        return user
+    }
+
+    @Patch (':id') // PATCH /users/;id
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return { id, ...userUpdate }
+    }
+
+    @Delete (':id') // GET /users/;id
+    delete(@Param('id') id: string) {
         return { id }
     }
 
