581c60f817edd1f13e36b3b7aeec3b5994c9cc0f
Eli4545 <elisimon4545@gmail.com>
2024-08-18 14:03:33 -0400

    applied validation to the Dto's

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,16 @@
+import { IsEmail, IsEnum, IsNotEmpty, isString, IsString } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
     name : string;
+
+    @IsEmail()
     email : string;
+
+    @IsEnum([ "INTERN","ENGENEER" ,'ADMIN'],{
+        message : 'Valid role required'
+    })
     role : "INTERN" | "ENGENEER" | 'ADMIN';
 
 }
